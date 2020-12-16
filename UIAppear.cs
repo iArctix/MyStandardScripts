04bbe7331a2885faef40c1f266bf9490f237df1c
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 11:50:52 +0000

    Add files via upload
    Ui prompt. For example when approaching a interactable item a ui prompt will pop up saying what to do to interact.

---
+++ UIAppear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField] private Image customeImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            customeImage.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customeImage.enabled = false;
+        }
+    }
+
+
+
+
+
+
+
+
+}
+	
