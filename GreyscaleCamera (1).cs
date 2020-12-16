43f90339a8e1855b2a4d3cbc8c6e30ade57f083e
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 12:06:30 +0000

    Black and white shader
    Greyscale shader file dragged onto material. camera is applied to a  a camera to create a nice black and white shader effect in scene.

---
+++ GreyscaleCamera (1).cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyscaleCamera : MonoBehaviour
+{
+
+    public Material greyscaleMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, greyscaleMaterial);
+    }
+}
