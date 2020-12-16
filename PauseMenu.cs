d1264d502162e7b47c7d1475829b06e8707a5686
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 11:58:04 +0000

    Pause Menu
    Basic Pause menu uses ui and code for it to work.

---
+++ PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+   
+    private void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+        AudioListener.pause = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        AudioListener.pause = false;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+}
+
+
+        
