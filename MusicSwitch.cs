6c2582462f91afc158ab6b0e4becd1ff4c5d7b37
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 11:46:28 +0000

    Add files via upload
    Music Switch. Drag Light source into inspector to use. Can be used for light switches , lamps etc.

---
+++ MusicSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitch : MonoBehaviour
+{
+
+    public  AudioSource AS;
+    private bool on = false;
+
+    // Use this for initialization
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on )
+        {
+            AS.enabled = true;
+            AS.Play();
+            on = true;
+        }
+        else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && on)
+        {
+            AS.Stop();
+            AS.enabled = false;
+            on = false;
+        }
+    }
+}
+
