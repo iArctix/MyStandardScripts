80f495bf9e45ffc21bbb529b1144969cc1cd3d51
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 12:01:18 +0000

    Light Switch

---
+++ TurnLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour
+{
+
+    public GameObject Lamp;
+    private bool on = false;
+    public AudioSource lighton;
+    public AudioSource lightoff;
+
+    // Use this for initialization
+
+    private void Start()
+    {
+        Lamp.SetActive(false);
+    }
+
+
+
+
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on)
+        {
+            Lamp.SetActive(true);
+            on = true;
+            lighton.Play();
+            
+        }
+        else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && on)
+        {
+            Lamp.SetActive(false);
+            on = false;
+            lightoff.Play();
+
+        }
+    }
+}
