00d7556717ed3258f55cca66d513b781cd332a55
iArctix <76104808+iArctix@users.noreply.github.com>
2020-12-16 11:54:59 +0000

    Random Spawn
    Allows you to make an object have a random spawn from a  list of  predetermined spawn points in inspector.

---
+++ spawnrandom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnrandom : MonoBehaviour {
+    public List<Vector3> spawnPoints = new List<Vector3>();
+
+	// Use this for initialization
+	void Start () {
+
+        Spawn();
+	}
+	
+	// Update is called once per frame
+	public void Spawn()
+    {
+        transform.position = spawnPoints[Random.Range(0, spawnPoints.Count + 1)];	
+	}
+}
